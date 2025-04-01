b58e8d18591a75027eb865b4e87c36d6ba2b9046
emmaashi <emmashi2006@gmail.com>
2025-03-31 21:25:54 -0400

    install framer motion + favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,11 @@ const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Emma Shi",
-}
+  icons: {
+    icon: [{ url: "/favicon.ico" }],
+    apple: [{ url: "/apple-icon.png", sizes: "180x180", type: "image/png" }],
+  },
+};
 
 export default function RootLayout({
   children,
@@ -17,6 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} disableTransitionOnChange={false}>
           {children}
